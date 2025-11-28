1e00c76c56b2327665e2260379f5fdd41b8e0b1a
Pratik Giri <pratikgiri585@gmail.com>
2025-11-29 03:23:09 +0530

    conditional  operation js

---
+++ Javascript/condition.js
@@ -0,0 +1,14 @@
+if(-1){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+if(true&&false){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
