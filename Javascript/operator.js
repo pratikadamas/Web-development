1d0e882e8a89c8de4838f80b46ca5cbe29b176df
Pratik Giri <pratikgiri585@gmail.com>
2025-11-29 01:52:18 +0530

    operator in js

---
+++ Javascript/operator.js
@@ -0,0 +1,51 @@
+// // arithmatic operators 
+// console.log(5 + 3); // Addition
+// console.log(10 - 2);; // Subtraction
+// console.log(4 * 2); // Multiplication
+// console.log(8 / 2);
+// console.log(7 % 3); // Modulus
+// console.log(2 ** 3); // Exponentiation
+
+// // assignment operators
+// let x = 10; // Assignment
+// x += 5;
+// x -= 3;
+// x *= 2;
+// x /= 4;
+// x %= 3;
+// console.log(x);
+// x **= 2;
+// console.log(x);
+// // comparison operators
+// console.log(5 == '5'); // Equal to
+// console.log(5 === '5'); // Strict equal to
+// console.log(5 != '5'); // Not equal to
+// console.log(5 !== '5'); // Strict not equal to
+// console.log(5 > 3);
+// console.log(5 < 3);
+// console.log(5 >= 5);
+// console.log(3 <= 5);
+// // logical operators
+
+// //logical AND
+// console.log(true && false); 
+// console.log(false && false);// Logical AND
+// console.log(false &&false)
+// console.log(true&&true)
+
+// console.log(10&&0)
+// console.log(0&&10)
+// console.log(10&&5)
+// console.log(-1&&5)
+
+console.log(10 && null && false)
+
+// logical OR
+console.log(true || false);
+console.log(false || false);// Logical OR
+console.log(false || true)
+console.log(true||true)
+
+// bitwise operators
+console.log(5 & 3);
+console.log(5 | 3);
