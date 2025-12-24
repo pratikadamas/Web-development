4b06f7719a92754c142aa80c6a1c15205f3a074c
Pratik Giri <pratikgiri585@gmail.com>
2025-12-24 23:46:31 +0530

    react

---
+++ React Js/02-component/react component/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">My Website</h1>
+
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ React Js/02-component/react component/src/components/card.jsx
@@ -0,0 +1,10 @@
+
+function Card() {
+  return (
+    <div className="card">
+      <h2>This is Card Component</h2>
+    </div>
+  );
+}
+
+export default Card;
