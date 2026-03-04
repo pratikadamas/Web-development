a688bd9714da03f9e84ca18ed29aab4349120751
Pratik Giri <pratikgiri585@gmail.com>
2026-03-05 01:53:53 +0530

    ...

--- React Js/01-react/src/App.jsx
+++ React Js/01-react/src/App.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
-//rafc
+// write functional component in react
+import Chai from "./Chai"
+import { useState } from "react"
 
-export const App = () => {
+// hooks are used in functional component to manage state and side effects
+
+
+function App() {
+
+  const [user, setUser] = useState("John Doe")// useState is a hook that returns an array with two elements: the current state value and a function to update it. We can use array destructuring to assign these values to variables. In this case, we are initializing the user state with the value "John Doe" and providing a function setUser to update it.
+  const age=25
+  console.log(user)
+
+  function handleClick(){
+    // alert("button is clicked")
+    setUser("pratik giri")
+  }
+
   return (
-
-<>
-  <div className="papa">
-    <h1>Hello, React! I am Pratik Giri  </h1>
-  </div>
-
-  <div className="chacha">
-    <h2>This is a simple React fragment.</h2>
-  </div>
-</>
-
+    <div>
+      <h1>user name is {user} and value of age is {age}
+      </h1>
+      <Chai />
+      <button onClick={handleClick}>click</button>
+    </div>
   )
 }
+
 export default App
-
-
-
---
+++ React Js/01-react/src/Chai.jsx
@@ -0,0 +1,11 @@
+function Chai() {
+  return (
+    <>
+      <h1>Chai</h1>
+      <p>Chai is ready.</p>
+      <p>By Pratik Giri</p>
+    </>
+  );
+}
+
+export default Chai;
